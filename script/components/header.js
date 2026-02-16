c4666c3f4a8626e47124d4c554f6d07e4b64fc32
Rebecka Kinn <rebeckaki@getacademy.no>
2026-02-16 14:30:53 +0100

    adding menu in header

--- script/components/header.js
+++ script/components/header.js
@@ -1,11 +1,23 @@
 function header(){
     return /*HTML*/`
     <section>
-        <div class="logo-image click" onclick="changePage('home')">
-            <img src="./img/logo/main_logo_light.svg" loading="lazy" alt="">
+        <div class="header-logo">
+            <div class="logo-image click" onclick="changePage('home')">
+                <img src="./img/logo/main_logo_light.svg" loading="lazy" alt="">
+            </div>
+            <h1 class="statement-font remove-margin">Rebecka Kinn</h1>
         </div>
-        <h1 class="statement-font remove-margin">Rebecka Kinn</h1>
+
+
+        <ul class="button-container">
+            <li>
+                <button onclick="changePage('resume')" class="link_tag click">resume</button>
+            </li>
+            <span class="link_tag">&#124;&#124;</span>
+            <li>
+                <button onclick="changePage('coverLetter')" class="link_tag click">cover letter</button>
+            </li>
+        </ul>
     </section>
     `;
 }
-
--- script/components/projects.js
+++ script/components/projects.js
@@ -8,7 +8,7 @@ function projectCards(){
             <div class="card_information">
                 <h1>${project.title}</h1>
                 <p>${project.description}</p>
-                <div class="button-container">
+                <div class="button-container bottom">
                     <a class="link_tag click center" href=${project.repoLink} target="_blank">View the code</a>
                     <span class="link_tag">&#124;&#124;</span>
                     <a class="link_tag click center" href=${project.demoLink} target="_blank">Open demo</a>
