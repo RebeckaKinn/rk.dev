70bdcb70c3cd651d27e418290b6d2e5e9e67a483
Rebecka Kinn <rebeckaki@getacademy.no>
2026-02-16 10:44:12 +0100

    added data and image for the other assignments

---
+++ script/data/data.js
@@ -0,0 +1,27 @@
+const projectData = [
+    {
+        id: 1,
+        thumbnail: "./img/projects/raindydays_thumbnail.jpg",
+        title: "Rainydays",
+        description: "A modern and light designed website for outdoor clothing. Keeping it modern and simple while enhances the thrill of the outdoors.",
+        repoLink: "https://github.com/RebeckaKinn/html-css-ca-rebecka-kinn",
+        demoLink: "https://rebeckakinn.github.io/html-css-ca-rebecka-kinn/home.html",
+    },
+    {
+        id: 2,
+        thumbnail: "./img/projects/playbox_thumbnail.jpg",
+        title: "Playbox",
+        description: "An online game store, witn inspiration from Steam and Gamestop.",
+        repoLink: "https://github.com/RebeckaKinn/JS1_Course_Assignment",
+        demoLink: "https://rebeckakinn.github.io/JS1_Course_Assignment/",
+    },
+    {
+        id: 3,
+        thumbnail: "./img/projects/museum_thumbnail.jpg",
+        title: "Science Museum",
+        description: "An informative museum-website with families in mind. Fun and playful design while holding onto the classic museum-feel.",
+        repoLink: "https://github.com/RebeckaKinn/semester_project_1_museum",
+        demoLink: "https://rebeckakinn.github.io/semester_project_1_museum/",
+    },
+
+];
