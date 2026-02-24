0e54b08ac8c2a72272fca0b38adeafe21aff5101
Rebecka Kinn <rebeckaki@getacademy.no>
2026-02-24 15:04:20 +0100

    adding new image and updating files

--- script/setup.js
+++ script/setup.js
@@ -1,21 +1,22 @@
-let page = "about"; //for testing. "home" for default
+let page = "home";
 
-function drawPage(){
-    document.getElementById('app').innerHTML = /*HTML*/`
+function drawPage() {
+  document.getElementById("app").innerHTML = /*HTML*/ `
         <header>${header()}</header>
         <main>${createMainContent()}</main>
         <footer>${footer()}</footer>
     `;
 }
 
-function createMainContent(){
-    if(page == "home") return mainPage();
-    else if(page == "about") return about();
-    else if(page == "resume" || page == "coverLetter") return documentationPage(page)
-    else return errorPage();
+function createMainContent() {
+  if (page == "home") return mainPage();
+  else if (page == "about") return about();
+  else if (page == "resume" || page == "coverLetter")
+    return documentationPage(page);
+  else return errorPage();
 }
 
-function changePage(newPage){
-    page = newPage;
-    drawPage();
+function changePage(newPage) {
+  page = newPage;
+  drawPage();
 }
